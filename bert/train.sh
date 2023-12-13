f5119bdfe3ff67648f029c811a73271fce047427
Omar OUARDIGHI <65140778+Omar-Ouardighi@users.noreply.github.com>
2023-12-13 11:05:05 +0100

    Update train.sh

--- bert/train.sh
+++ bert/train.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -l
-#SBATCH -J BRATS            # Display Name
+#SBATCH -J Bert            # Display Name
 #SBATCH -N 1                # Number of nodes
 #SBATCH -c 7                # Cores assigned to each tasks
 #SBATCH -p gpu              # Job-type, can be batch (only CPU) or gpu
 #SBATCH -G 1                # Number of GPUs for the job
 #SBATCH --time=0-30:00:00   # Time limit
-#SBATCH --array=0-3         # Definition of job array
 #SBATCH --qos normal
 
 
